7839350e50056ea9f508e2b1afdf0d3924204850
Naren404 <narendrasunar47@gmail.com>
2023-06-25 12:59:01 +1000

    full CRUD for book

--- api/controllers/booksController.js
+++ api/controllers/booksController.js
@@ -1,32 +1,64 @@
 import Book from "../models/bookModel.js"
 
 // INDEX
-const getBooks = async(req, res) => {
-  const books = await Book.find()
-
-  if(books){
-    return res.json({
-      books
-    })
-  }else{
-    return res.status(400).send({error: "Cannot find books"})
+const getBooks = async (req, res) => {
+  try {
+    const books = await Book.find()
+    res.json(books)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
   }
 }
 
-const createBook = async(req, res) => {
-  const {title, author} = req.body
-
-  const book = await Book.create({ title, author })
-
-  if(book){
-    return res.json({
-      title: book.title,
-      author: book.author
-    })
-  }
-  else{
-    return res.status(400).send({error: "Cannot create book"})
+// SHOW
+const getBook = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+    console.log('book', book)
+    res.json(book)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
   }
 }
 
-export { getBooks, createBook }
+// CREATE
+const createBook = async (req, res) => {
+  try {
+    const newBook = new Book(req.body)
+    await newBook.save()
+
+    res.json(newBook)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const updateBook = async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    if(!updatedBook){
+      res.status(400).json({ error: 'Item not found' })
+    }
+
+    res.json(updatedBook)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const deletebook = async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id)
+
+    if(!deletedBook){
+      res.status(400).json({ error: 'Item not found' })
+    }
+
+    res.json({ message: 'Book deleted' })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+export { getBook, getBooks, createBook, updateBook, deletebook }
--- api/routes/bookRoutes.js
+++ api/routes/bookRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { createBook, getBooks } from "../controllers/booksController.js";
+import { createBook, deletebook, getBook, getBooks, updateBook } from "../controllers/booksController.js";
 
 const router = express.Router()
 
 router.route('/').get(protect, getBooks)
-router.route('/create').post(protect, createBook)
+router.route('/:id').get(protect, getBook)
+router.route('/').post(protect, createBook)
+router.route('/:id').put(protect, updateBook)
+router.route('/:id').delete(protect, deletebook)
 
 export default router
